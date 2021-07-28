ebc6b2e354335d456533b8debb3dcbd149b7b1a9
LAymeric <aymeric.larvet@gmail.com>
2021-07-28 12:45:46 +0200

    refacto factory

--- ESGI.DesignPattern.Projet/AttributeDescriptorFactory.cs
+++ ESGI.DesignPattern.Projet/AttributeDescriptorFactory.cs
@@ -2,31 +2,41 @@
 
 namespace ESGI.DesignPattern.Projet
 {
-    public abstract class AttributeDescriptorFactory
+    public static class AttributeDescriptorFactory
     {
         public static AttributeDescriptor Create(string descriptorName, Type mapperType, Type forType)
         {
-            if (isDefaultDescriptor(forType))
+            switch (forType.Name)
             {
-                return new DefaultDescriptor(descriptorName, mapperType, forType);
+                case "Int32":
+                case "DateTime":
+                    return new DefaultDescriptor(descriptorName, mapperType, forType);
+                case "User":
+                    return new ReferenceDescriptor(descriptorName, mapperType, forType);
+                default:
+                    throw new Exception($"invalid type for : {forType}");
             }
             
-            if (isReferenceDescriptor(forType))
-            {
-                return new ReferenceDescriptor(descriptorName, mapperType, forType);
-            }
-
-            throw new Exception($"invalid type for : {forType}");
+            // if (IsDefaultDescriptor(forType))
+            // {
+            //     return new DefaultDescriptor(descriptorName, mapperType, forType);
+            // }
+            //
+            // if (IsReferenceDescriptor(forType))
+            // {
+            //     return new ReferenceDescriptor(descriptorName, mapperType, forType);
+            // }
+            //throw new Exception($"invalid type for : {forType}");
         }
         
-        private static bool isDefaultDescriptor(Type type)
-        {
-            return type == typeof(int) || type == typeof(DateTime);
-        }  
-        
-        private static bool isReferenceDescriptor(Type type)
-        {
-            return type == typeof(User);
-        }
+        // private static bool IsDefaultDescriptor(Type type)
+        // {
+        //     return type == typeof(int) || type == typeof(DateTime);
+        // }  
+        //
+        // private static bool IsReferenceDescriptor(Type type)
+        // {
+        //     return type == typeof(User);
+        // }
     }
 }
